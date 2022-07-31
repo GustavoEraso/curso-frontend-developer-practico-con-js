22d75366f8de5e9c97b6c3489cf9decbb29640f3
Gustavo Eraso <gustavoeraso1988@gmail.com>
2022-07-31 03:02:02 -0300

    se logro modificar el aside derecho dependiedo de que card era seleccionada

--- main.js
+++ main.js
@@ -4,11 +4,21 @@ const hamburguesa= document.querySelector(".menu");
 const menuMobile = document.querySelector(".mobile-menu");
 const carrito= document.querySelector(".navbar-shopping-cart");
 const producDetailLeftAside = document.querySelector(".product-detail-left-aside");
+const producDetailRightAside = document.querySelector(".product-detail-right-aside");
+const producDetailRightAsideClouseButon = document.querySelector(".product-detail-right-aside-close");
 const cardsContainer = document.querySelector(".cards-container");
+
+const producDetailRightAsideImg = document.querySelector(".product-detail-right-aside-img");
+const producDetailRightAsidePrice = document.querySelector(".product-info-right-aside-price");
+const producDetailRightAsideName = document.querySelector(".product-info-right-aside-name");
+const producDetailRightAsideDescription = document.querySelector(".product-info-right-aside-description");
+
+
 
 menuEmail.addEventListener("click", function(){toggleDesktopMenu(desktopMenu)});
 hamburguesa.addEventListener("click", function(){toggleDesktopMenu(menuMobile)});
 carrito.addEventListener("click", function(){toggleDesktopMenu(producDetailLeftAside)});
+producDetailRightAsideClouseButon.addEventListener("click", function(){toggleDesktopMenu(producDetailRightAside)});
 
 function toggleDesktopMenu(elemento){
     
@@ -24,28 +34,60 @@ function toggleDesktopMenu(elemento){
 
         if(!producDetailLeftAside.classList.contains("inactive"))
         {producDetailLeftAside.classList.add("inactive")};
+        
+        if(!producDetailRightAside.classList.contains("inactive"))
+        {producDetailRightAside.classList.add("inactive")};
 
         elemento.classList.toggle("inactive");
         }
     }
+
+    function openRightAside(elemento){         
+
+        
+    
+          if(elemento.classList.contains("inactive"))
+          {elemento.classList.remove("inactive")}; 
+        
+          if(!menuMobile.classList.contains("inactive"))
+          {menuMobile.classList.add("inactive")};
+  
+          if(!desktopMenu.classList.contains("inactive"))
+          {desktopMenu.classList.add("inactive")};
+  
+          if(!producDetailLeftAside.classList.contains("inactive"))
+          {producDetailLeftAside.classList.add("inactive")};
+           
+         
+          
+          }
+
+          
+        
+        
 
     const productList =[];
     
     productList.push({
         name:'Bike',
         price: 120,
-        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+        descripcion: 'esta es una descripcion'
     });
     productList.push({
         name:'Telefono',
         price: 820,
-        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+        image: 'https://images.pexels.com/photos/699122/pexels-photo-699122.jpeg',
+        descripcion: 'esta es otra'
     });
     productList.push({
         name:'Televisor',
         price: 1120,
-        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+        image: 'https://images.pexels.com/photos/4009402/pexels-photo-4009402.jpeg',
+        descripcion: 'esta es la ultima'
     });
+
+    
 
     function renderProducts(arr){
 
@@ -62,13 +104,16 @@ function toggleDesktopMenu(elemento){
         </div>
       </div> */
 
-
-    for (product of arr){
+      
+    for (const product of arr){
+       
+         
         const productCard= document.createElement('div');
         productCard.classList.add('product-card');
         
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.image);
+        // productImg.addEventListener('click',function(){openRightAside(producDetailRightAside)});
         
         const productInfo= document.createElement('div');
         productInfo.classList.add('product-info');
@@ -96,10 +141,36 @@ function toggleDesktopMenu(elemento){
         productCard.appendChild(productInfo);
 
         cardsContainer.appendChild(productCard);
+
+        
+
+        productImg.addEventListener('click', function(){
+          mostrarInfoProduct(product.image, product.price, product.name, product.descripcion)
+          openRightAside(producDetailRightAside)
+          console.log(product.name)
+        });
+
+        
     }
 };
 
 renderProducts(productList);
+
+function mostrarInfoProduct(imagen, precio, nombre, descripcion){
+  
+  
+  producDetailRightAsideImg.setAttribute('src', imagen)
+  producDetailRightAsidePrice.innerText = '$' + precio
+  producDetailRightAsideName.innerText = nombre
+  producDetailRightAsideDescription.innerText = descripcion
+
+  // productoACarrito = []
+  // productoACarrito.push({
+  //     name: nombre,
+  //     price: precio,
+  //     image: imagen
+  // })
+}
     
 
     
